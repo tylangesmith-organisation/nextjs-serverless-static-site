a351798c3adcfc5fefab534b8e24f4370b669396
Ty Lange-Smith <tylangesmith@gmail.com>
2022-01-30 19:47:52 +1100

    Update CloudFront mapping Function name

--- .github/deploy/src/stack.ts
+++ .github/deploy/src/stack.ts
@@ -64,7 +64,7 @@ export default class StaticWebsiteStack extends Stack {
     // the Next.js library isn't loaded on the client
     const functionAssociation = createFunction({
       scope: this,
-      name: `mapping-${branchName}`,
+      name: `mapping-${branchedSubDomainName}`,
       filePath: './src/functions/mapperFunction.js'
     })
 
