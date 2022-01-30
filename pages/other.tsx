be9235a96cf86814de8f56ffd260a1460877dd07
Ty Lange-Smith <tylangesmith@gmail.com>
2022-01-30 11:05:53 +1100

    Add footer component

--- pages/other.tsx
+++ pages/other.tsx
@@ -2,29 +2,31 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import Head from 'next/head'
 
-const Other: NextPage = () => {
+import Footer from '../components/footer'
+
+const Home: NextPage = () => {
   return (
-    <div>
+    <div className="flex flex-col h-screen break-words">
       <Head>
         <title>Static Site Example</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="container px-2 h-screen flex flex-col justify-center items-center prose mx-auto">
-        <h1 className="text-5xl font-black">
-          Other Page
-        </h1>
-        <p className="text-2xl text-center">
-          This is an example Next.js static site for the blog<br /><a className="text-blue-600" href="https://tylangesmith.com/blog/building-a-nextjs-serverless-static-site-using-aws-cdk">Building a Next.js Serverless Static Site using AWS CDK</a>
-        </p>
-        <Link href="/">
-          <a className="text-2xl">
-            Home Page
-          </a>
-        </Link>
-      </main>
+        <main className="container px-2 flex-grow flex flex-col justify-center items-center prose mx-auto">
+          <h1 className="text-5xl font-black">
+            Other Page
+          </h1>
+          <p className="text-2xl text-center">
+            This is an example Next.js static site for the blog<br /><a className="text-blue-600" href="https://tylangesmith.com/blog/building-a-nextjs-serverless-static-site-using-aws-cdk">Building a Next.js Serverless Static Site using AWS CDK</a>
+          </p>
+          <Link href="/">
+            <a className="text-2xl">
+              Home Page
+            </a>
+          </Link>
+        </main>
+        <Footer />
     </div>
   )
 }
 
-export default Other
+export default Home
